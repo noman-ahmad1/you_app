8b0d9f9b98bf4d21f7af6477058bfee30ac1bdb0
Noman Ahmad <116165547+noman-ahmad1@users.noreply.github.com>
2026-04-19 19:59:06 +0500

    Clear sensitive data from env.dart
    Removed sensitive API keys and URLs from the environment configuration.

--- lib/env.dart
+++ lib/env.dart
@@ -2,7 +2,7 @@ class Environments {
   static const String appName = 'You';
   // static const String googleMapsKey = 'GOOGLE-MAPS-KEY';
   static const String googleGeminiKey =
-      'AIzaSyDrWZydDwLuF0WA4LJooxdDBoNYHpmLaGY';
-  static const String apiBaseURL = 'https://api.youtaicosmetic.net/';
-  static const String websiteURL = 'https://multi-salon.initappz.com/';
+      '';
+  static const String apiBaseURL = '';
+  static const String websiteURL = '';
 }
